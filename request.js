da4f47d35cc772902160410f8e038847ea76a012
HUANGDYANG <1159973635@qq.com>
2021-06-28 09:51:23 +0800

    request 正则

--- request.js
+++ request.js
@@ -1,89 +1,20 @@
-import wepy from 'wepy'
+/*
+ * @Author: your name
+ * @Date: 2021-06-28 09:00:58
+ * @LastEditTime: 2021-06-28 09:51:00
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \JS\request.js
+ */
 const baseUrl = 'https://xcx.paoding.cc'
-// https://xcx.paoding.cc
-// http://192.168.1.232:8082
 const isDebug = true
 
-//  拦截器请求配置 app.wpy => constructor中配置
-const requestConfig = {
-  // 发出请求时的回调函数
-  config(p) {
-    wx.showNavigationBarLoading()
-    p.timestamp = +new Date()
-    // debugger
-    if (this.globalData.accessToken || wx.getStorageSync('accessToken')) {
-      p.header = Object.assign({}, p.header, {
-        accessToken: this.globalData.accessToken || wx.getStorageSync('accessToken')
-      })
-    }
-    // isDebug && console.log('config request: ', p)
-    return p
-  },
-  success(p) {
-    // 可以在这里对收到的响应数据对象进行加工处理
-    isDebug && console.info('request success: ', p.data)
-    let statusCode = p.statusCode + ''
-    let code = p.data.code + ''
-    // statusCode.startsWith('2') && p.data.code === 9000
-    if (statusCode.startsWith('2') && p.data.code === 1008) {
-      wepy
-        .showModal({
-          content: '请先进行登录操作', // 提示的内容,
-          showCancel: false, // 是否显示取消按钮,
-          confirmText: '确定', // 确定按钮的文字，默认为取消，最多 4 个字符,
-          confirmColor: '#2565C3' // 确定按钮的文字颜色,
-        })
-        .then(res => {
-          wx.redirectTo({
-            url: '/pages/wxInfo'
-          })
-        })
-      return false
-    }
-
-    if (statusCode.startsWith('2') && !code.startsWith('0')) {
-      if (code === '404') {
-        return false
-      }
-      // if (code === '9999') {
-      //   return false
-      // }
-      // if (code === '500') {
-      //   return false
-      // }
-      // console.log(statusCode, code)
-      wx.showToast({
-        icon: 'none',
-        title: p.data.message,
-        duration: 1000
-        // mask: true
-      })
-      return false
-    }
-    // 必须返回响应数据对象，否则后续无法对响应数据进行处理
-    return p.data
-  },
-  fail(p) {
-    wx.showToast({
-      icon: 'none',
-      title: p.data.message,
-      duration: 1000
-      // mask: true
-    })
-    isDebug && console.log('request fail: ', p)
-    return p
-  },
-  complete(p) {
-    // isDebug && console.log('request complete: ', p)
-    wx.hideNavigationBarLoading()
-  }
-}
 
 const $post = (url, data, contentType = false) => {
   isDebug && console.info('请求的数据', data, '请求的接口', url)
   contentType ? (contentType = 'application/x-www-form-urlencoded;') :
     (contentType = 'application/json;charset=UTF-8')
-  return wepy.request({
+  return we.request({
     url: baseUrl + url, // 开发者服务器接口地址",
     data: data, // 请求的参数",
     method: 'POST',
@@ -97,7 +28,7 @@ const $post = (url, data, contentType = false) => {
 
 const $get = (url, data) => {
   isDebug && console.info('请求的数据', data, '请求的接口', url)
-  return wepy.request({
+  return we.request({
     url: baseUrl + url,
     header: {
       siteId: '2'
@@ -164,7 +95,6 @@ const api = {
 }
 
 export {
-  requestConfig,
   baseUrl,
   isDebug,
   $post,
--- validate.js
+++ validate.js
@@ -1,7 +1,11 @@
-export function isvalidUsername(str) {
-  return true
-}
-
+/**
+ * @description: 安装vscode中插件搜索框输入any-rule
+安装完毕后按F1(或者ctrl+shift+p).
+输入zz可以看到正则列表.
+或者输入关键词, 比如"手机".
+ * @param {*}
+ * @return {*}
+ */
 /* 合法uri */
 export function validateURL(textval) {
   const urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/
